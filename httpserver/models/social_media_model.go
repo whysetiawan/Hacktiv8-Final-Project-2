ad6c9b58239ca8eef7d5c86bc78808946a9904ab
wahyu.setiawan <wahyu.setiawan@sicepat.com>
2022-11-15 21:36:17 +0700

    feat:fix modelling

--- httpserver/models/social_media_model.go
+++ httpserver/models/social_media_model.go
@@ -2,7 +2,6 @@ package models
 
 type SocialMediaModel struct {
 	BaseModel
-	SocialMediaId  uint   `gorm:"primaryKey;column:id" json:"social_media_id"`
 	Name           string `json:"name"`
 	SocialMediaUrl string `json:"social_media_url"`
 	UserId         uint   `json:"user_id"`
--- httpserver/repositories/social_media_repository.go
+++ httpserver/repositories/social_media_repository.go
@@ -19,7 +19,7 @@ func NewSocialMediaRepository(db *gorm.DB) *socialMediaRepository {
 }
 
 func (r *socialMediaRepository) CreateSocialMedia(socialMedia *models.SocialMediaModel) (*models.SocialMediaModel, error) {
-	err := r.db.Create(socialMedia).Error
+	err := r.db.Create(&socialMedia).Error
 	if err != nil {
 		return socialMedia, err
 	}
