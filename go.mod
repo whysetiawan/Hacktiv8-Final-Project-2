b0506880dc980d3454d8e30a3fda03a55254d6bb
wahyu.setiawan <wahyu.setiawan@sicepat.com>
2022-11-16 01:56:13 +0700

    update unit test

--- go.mod
+++ go.mod
@@ -14,6 +14,12 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
@@ -45,6 +51,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/stretchr/testify v1.8.1
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
--- utils/auth_helper_test.go
+++ utils/auth_helper_test.go
@@ -2,20 +2,47 @@ package utils_test
 
 import (
 	"final-project-2/httpserver/models"
+	"final-project-2/utils"
 	"testing"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
 )
 
+const mockSecretKey = "Rebahaners"
+
 func TestNewAuthHelper(t *testing.T) {
-	userModel := models.UserModel{
+	helper := utils.NewAuthHelper(mockSecretKey)
+	assert.NotEmpty(t, helper.JWT_SECRET_KEY)
+	assert.Equal(t, helper.JWT_SECRET_KEY, mockSecretKey)
+}
+
+func TestGenerateToken(t *testing.T) {
+	helper := utils.NewAuthHelper(mockSecretKey)
+
+	user := models.UserModel{
 		BaseModel: models.BaseModel{
 			ID: 1,
 		},
-		Username: "Testing",
+		Username: "test",
 		Email:    "test@email.com",
 		Password: "123qweasd",
-		Age:      23,
+		Age:      70,
 	}
 
-	// accessToken, refreshToken, err := utils.NewAuthHelper().GenerateToken(&userModel)
+	accessToken, refreshToken, err := helper.GenerateToken(&user)
 
+	assert.NoError(t, err)
+	assert.NotEmpty(t, accessToken)
+	assert.NotEmpty(t, refreshToken)
+
+	isValid, payload, err := helper.VerifyToken(accessToken)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, payload)
+	assert.True(t, isValid)
+
+	userModel := helper.JwtClaimsToUserModel(payload.(jwt.MapClaims))
+	assert.NotZero(t, userModel.ID)
+	assert.Equal(t, userModel, user)
 }
